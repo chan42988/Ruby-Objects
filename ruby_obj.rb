33fbe30d3dec3a26a8e6722d9e14358c2c729072
Christopher <chan42988@gmail.com>
2016-04-24 19:19:59 -0400

    final touches

--- ruby_obj.rb
+++ ruby_obj.rb
@@ -1,19 +1,29 @@
 #Calculator
 class Calculator
-  def initialize
-    value1 = 10
-    value2 = 5
-    @add = value1 + value2
-    @sub = value1 - value2
-    @mul = value1 * value2
-    @div = value1 / value2
-    puts @add
-    puts @sub
-    puts @mul
-    puts @div
-  end
+
+  attr_accessor :value1, :value2
+
+    def initialize(value1, value2)
+    @value1 = value1
+    @value2 = value2
+    end
+
+    def add
+    @value1 + @value2
+    end
+
+    def subtract
+    @value1 - @value2
+    end
+
+    def mult
+    @value1 * @value2
+    end 
+
+    def div
+    @value1 / @value2
+    end
 end
-Calculator.new
 
 
 #Elevator 
@@ -22,40 +32,40 @@ class Elevator
   attr_accessor :floor
   attr_reader :ud, :music
 
-  def initialize(floor)
-    @floor = floor
-    @ud = false
-    @music = false
-  end 
+    def initialize(floor)
+      @floor = floor
+      @ud = false
+      @music = false
+    end 
 
-#which floor you are on
-  def cfloor(new_floor)
-      @floor = new_floor
-  end
-
-#up or down
-  def updown
-      @ud = !@ud
-      if @ud
-          "going up"
-      else
-          "going down"
-      end
-  end 
-
-#cheery greeting and current floor
-  def greeting
-    "Greetings! You are on " + "#{@floor}"
-  end
-
-#music
-  def music
-    @music = !@music
-    if @music
-      "You are now listening to music"
-    else
-      "You turned off music"
+  #current floor
+    def cfloor(new_floor)
+        @floor = new_floor
     end
-  end
+
+  #up or down
+    def updown
+        @ud = !@ud
+        if @ud
+            "going up"
+        else
+            "going down"
+        end
+    end 
+
+  #cheery greeting and current floor
+    def greeting
+      "Greetings! You are on " + "#{@floor}"
+    end
+
+  #music
+    def music
+      @music = !@music
+      if @music
+        "You are now listening to music"
+      else
+        "You turned off music"
+      end
+    end
 
 end
